5677eeaa9ff50733d4bb4af4db3160143049ada9
afrialivan <afrialivan0@gmail.com>
2023-03-09 20:04:09 +0800

    uypdate

--- resources/views/guest/register/catering.blade.php
+++ resources/views/guest/register/catering.blade.php
@@ -12,22 +12,22 @@
 
 <body class="bg">
 
-  <div class="tengah position-absolute">
+  <div class="d-block m-auto">
     <div class="logo">
-      <img class="d-block m-auto" src="img/logoo.svg" style="width: 150px;" alt="">
+      <img class="d-block m-auto" src="{{ asset('') }}" style="width: 150px;" alt="">
     </div>
     <div class="kotak bg-light d-block m-auto rounded-3 p-4 mt-3">
       <p class="h2">Daftar Catering !</p>
       <p class="h6">Data Pemilik</p>
       <form action="">
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Email</label>
               <input type="email" class="form-control" id="input">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Nomor Telepon</label>
               <input type="text" class="form-control" id="input">
@@ -35,13 +35,13 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Nama Pemilik</label>
               <input type="text" class="form-control" id="input">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">NIK (Nomor Induk Kependudukan) Pemilik</label>
               <input type="text" class="form-control" id="input">
@@ -49,13 +49,13 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Tanggal Lahir Pemilik</label>
               <input type="date" class="form-control" id="input">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Alamat Pemilik</label>
               <input type="text" class="form-control" id="input">
@@ -63,13 +63,13 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Upload Foto KTP Pemilik</label>
               <input class="form-control" type="file" id="formFile">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Upload Foto KTP bersama Pemilik</label>
               <input class="form-control" type="file" id="formFile">
@@ -78,13 +78,13 @@
         </div>
         <p class="h6">Data Pemilik</p>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Nama Catering</label>
               <input type="text" class="form-control" id="input">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Deskripsi Catering</label>
               <input type="text" class="form-control" id="input">
@@ -92,13 +92,13 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Logo Catering</label>
               <input class="form-control" type="file" id="formFile">
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Alamat Catering</label>
               <input type="text" class="form-control" id="input">
@@ -106,7 +106,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="input" class="form-label">Username</label>
               <input type="text" class="form-control" id="input">
@@ -114,7 +114,7 @@
           </div>
         </div>
         <div class="row align-items-end">
-          <div class="col-6">
+          <div class="col-12 col-lg-6 col-sm-6 col-md-6">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Kata Sandi</label>
               <input type="password" class="form-control" id="exampleInputPassword1">
