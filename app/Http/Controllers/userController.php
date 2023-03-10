e083a570e0247ca49d6aab05cd2d4954766b75cf
afrialivan <afrialivan0@gmail.com>
2023-03-10 15:15:26 +0800

    update

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -9,22 +9,24 @@ use Illuminate\Support\Facades\Auth;
 
 class userController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('guest.login.index');
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $validasi = $request->validate([
             'username' => 'required',
             'password' => 'required',
         ]);
 
-        if(Auth::attempt($validasi)) {
+        if (Auth::attempt($validasi)) {
             $request->session()->regenerate();
-            if(auth()->user()->role === 'admin') {
+            if (auth()->user()->role === 'admin') {
                 return redirect('/dashboard');
             }
-            if(auth()->user()->role === 'catering') {
+            if (auth()->user()->role === 'catering') {
                 return redirect('/dashboard');
             }
             return redirect('/catering');
@@ -32,58 +34,64 @@ class userController extends Controller
         return back();
     }
 
-    public function registerpelanggan() {
+    public function registerpelanggan()
+    {
         return view('guest.register.pelanggan');
     }
-    public function registercatering() {
+    public function registercatering()
+    {
         return view('guest.register.catering');
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->regenerateToken();
         $request->session()->invalidate();
         return redirect('/login');
     }
 
-    public function storeregistercatering(Request $request){
-//         $validateduser = $request->validate([
-//             "email" => ['required'],
-//             "tlp" => ['required'],
-//             "name" => ['required'],
-//             "nik" => ['required'],
-//             "tgl_lahir" => ['required'],
-//             "alamat" => ['required'],
-//             "username" => ['required', 'unique:users'],
-//             "password" => ['required'],
-//             "confirmpassword" => ['required','same:password'],
-//         ],
-//         [
-//             'tlp.required' => 'telepon harus di isi'
-//         ]
-//     );
+    public function storeregistercatering(Request $request)
+    {
+        $validateduser = $request->validate(
+            [
+                "email" => ['required'],
+                "tlp" => ['required'],
+                "name" => ['required'],
+                "nik" => ['required'],
+                "tgl_lahir" => ['required'],
+                "alamat" => ['required'],
+                "username" => ['required', 'unique:users'],
+                "password" => ['required'],
+                "confirmpassword" => ['required', 'same:password'],
+            ],
+            [
+                'tlp.required' => 'telepon harus di isi'
+            ]
+        );
 
-//     $validatedcatering = $request->validate([
-//         "ktp" => ['required'],
-//         "selfi_ktp" => ['required'],
-//         "namacatering" => ['required'],
-//         "deskripsi" => ['required'],
-//         "logo" => ['required'],
-//         "alamatcatering" => ['required'],
-//     ],
-//     [
-        
-//     ]
-// );
+        $validatedcatering = $request->validate(
+            [
+                "ktp" => ['required'],
+                "selfi_ktp" => ['required'],
+                "namacatering" => ['required'],
+                "deskripsi" => ['required'],
+                "logo" => ['required'],
+                "alamatcatering" => ['required'],
+                "slug" => ['required'],
+            ],
+        );
+        dd($validatedcatering['slug']);
 
-        $id = User::latest()->get();
-        dd($id[0]->id+1);
+        // $id = User::latest()->get();
+        // dd($id[0]->id+1);
 
         return redirect('/login')->with('proses', 'Akun anda telah berhasil dibuat, silahkan hubungi admin untuk verifikasi');
     }
 
-    public function profil() {
-        return view('user.profil.index',[
+    public function profil()
+    {
+        return view('user.profil.index', [
             'title' => 'Profil',
             'user' => User::where('id', auth()->user()->id)->get(),
         ]);
--- resources/views/guest/register/catering.blade.php
+++ resources/views/guest/register/catering.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register Catering</title>
-    <link rel="stylesheet" href="{{ asset('css/dashboard/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('dashboard/assets/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="css/register-catering.css">
 </head>
 
@@ -82,8 +82,9 @@
                 <div class="row">
                     <div class="col-12 col-lg-6 col-sm-6 col-md-6">
                         <div class="mb-3">
-                            <label for="input" class="form-label">Nama Catering</label>
-                            <input type="text" class="form-control" id="input" name="namacatering">
+                            <label for="nama" class="form-label">Nama Catering</label>
+                            <input type="text" class="form-control" id="nama" name="namacatering" value="">
+                            <input type="hidden" class="form-control" id="slug" name="slug">
                         </div>
                     </div>
                     <div class="col-12 col-lg-6 col-sm-6 col-md-6">
@@ -141,7 +142,7 @@
     </div>
 
 
-
+    <script src="js/register.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
