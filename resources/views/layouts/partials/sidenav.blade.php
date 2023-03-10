9fcc7f26fe9d8dd3d55b79c00b4ad123c186ee69
afrialivan <afrialivan0@gmail.com>
2023-03-10 12:08:22 +0800

    oi

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -12,19 +12,19 @@
       </a>
 
       <div class="nav-a position-relative">
-        <div class="bg-nav" id="bg-nav"></div>
+        {{-- <div class="bg-nav" id="bg-nav"></div> --}}
         <a class="text-decoration-none" href="/catering" id="catering">
-          <img src="{{asset('img/catering-aktif.svg')}}" alt="">
-          <h3 class="on-page">CATERING</h3>
+          <img src="{{asset('img/catering.svg')}}" alt="">
+          <h3 class="">CATERING</h3>
         </a>
-        <a class="text-decoration-none" href="/menu" id="menu">
+        {{-- <a class="text-decoration-none" href="/menu" id="menu">
           <img src="{{ asset('img/menu.png') }}" alt="">
           <h3>MENU</h3>
         </a>
         <a class="text-decoration-none" href="/keranjang" id="keranjang">
           <img src="{{ asset('img/keranjang.png') }}" alt="">
           <h3>KERANJANG</h3>
-        </a>
+        </a> --}}
         <a class="text-decoration-none" href="/pesanan" id="pesanan">
           <img src="{{ asset('img/pesanan.png') }}" alt="">
           <h3>PESANAN</h3>
